e79849522b187292eadd275596a05942de6393c8
Fábio simones <simones.fabio1994@gmail.com>
2025-03-11 16:26:11 -0300

    Anotações JPA para classes de domínio.

--- helpdesk-backend/src/main/java/com/fabiosimones/helpdesk/domain/Tecnico.java
+++ helpdesk-backend/src/main/java/com/fabiosimones/helpdesk/domain/Tecnico.java
@@ -3,15 +3,27 @@ package com.fabiosimones.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fabiosimones.helpdesk.domain.enums.Perfil;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Tecnico extends Pessoa{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		AddPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		AddPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {

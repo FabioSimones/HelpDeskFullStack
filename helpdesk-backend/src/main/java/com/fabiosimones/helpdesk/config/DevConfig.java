31097c44f26a6867ec03ebe7f118fe714ba5572b
Fábio simones <simones.fabio1994@gmail.com>
2025-03-13 19:19:36 -0300

    Criando perfil de desenvolvimento.

--- helpdesk-backend/src/main/java/com/fabiosimones/helpdesk/config/DevConfig.java
+++ helpdesk-backend/src/main/java/com/fabiosimones/helpdesk/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.fabiosimones.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,16 +9,22 @@ import org.springframework.context.annotation.Profile;
 import com.fabiosimones.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@Bean
 	public boolean instanciaDB() {
-		this.dbService.instanciaDB();
-		return true;
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		
+		return false;
 	}
 
 }

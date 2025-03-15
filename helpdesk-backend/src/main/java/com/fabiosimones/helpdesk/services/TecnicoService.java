5707df34ff8238edc8c591199ff47585661e888c
Fábio simones <simones.fabio1994@gmail.com>
2025-03-15 12:55:25 -0300

    Endpoint findAll para técnico.

--- helpdesk-backend/src/main/java/com/fabiosimones/helpdesk/services/TecnicoService.java
+++ helpdesk-backend/src/main/java/com/fabiosimones/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.fabiosimones.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,9 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return tecnicoRepository.findAll();
 	}
 }

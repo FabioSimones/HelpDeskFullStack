368784183b826df0e283fb2e1b70c0d25d5c7649
Fábio simones <simones.fabio1994@gmail.com>
2025-03-18 18:35:47 -0300

    Criando FindById de chamado.

---
+++ helpdesk-backend/src/main/java/com/fabiosimones/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.fabiosimones.helpdesk.services;
+
+import com.fabiosimones.helpdesk.domain.Chamado;
+import com.fabiosimones.helpdesk.repositories.ChamadoRepository;
+import com.fabiosimones.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+}

538e95ef4b6e4573597210c5ee0a4d76a5e2c510
Fábio simones <simones.fabio1994@gmail.com>
2025-04-07 20:33:21 -0300

    Componente Login criado.

--- helpdesk-frontend/helpdesk/src/app/app.module.ts
+++ helpdesk-frontend/helpdesk/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoComponent } from './components/tecnico/tecnico.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { TecnicoComponent } from './components/tecnico/tecnico.component';
     NavComponent,
     HomeComponent,
     HeaderComponent,
-    TecnicoComponent
+    TecnicoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

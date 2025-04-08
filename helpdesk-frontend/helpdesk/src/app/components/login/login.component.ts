a46a887dd0b4fb5d412ff5352a3d3883b7d6ca48
Fábio simones <simones.fabio1994@gmail.com>
2025-04-07 21:36:15 -0300

    Serviço de mensagens implementadas.

--- helpdesk-frontend/helpdesk/src/app/components/login/login.component.ts
+++ helpdesk-frontend/helpdesk/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
@@ -15,6 +16,17 @@ export class LoginComponent {
 
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
+
+  constructor(private toast: ToastrService){}
+
+  ngOnInit(): void{
+
+  }
+
+  logar(){
+    this.toast.error('Usuário e/ou senha inválidos.', 'Login')
+    this.creds.senha = '';
+  }
 
   validaCampos(): boolean{
     if(this.email.valid && this.senha.valid){

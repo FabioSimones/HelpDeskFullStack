bbff14717e02af406ce47696261d965ed342ba9e
Fábio simones <simones.fabio1994@gmail.com>
2025-04-16 18:49:59 -0300

    Rota para componente tecnico-create.

--- helpdesk-frontend/helpdesk/src/app/app-routing.module.ts
+++ helpdesk-frontend/helpdesk/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoComponent } from './components/tecnico/tecnico.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   {
@@ -23,6 +24,10 @@ const routes: Routes = [
       {
         path:'tecnicos',
         component: TecnicoComponent
+      },
+      {
+        path:'tecnicos/create',
+        component: TecnicoCreateComponent
       }
     ]
   },
